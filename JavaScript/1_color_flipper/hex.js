895068452facdda89b3706b9afecffa338000927
akadwa <kadazh123@gmail.com>
2023-01-23 16:34:14 +0200

    Add hex.js file and refactor comments

---
+++ JavaScript/1_color_flipper/hex.js
@@ -0,0 +1,6 @@
+// WHEN user clicks on the button,
+// THEN the background color of the body changes to a random color selected from an array WITH MORE VARIETY.
+//      The color name will also be displayed to the user.
+
+
+
--- JavaScript/1_color_flipper/script.js
+++ JavaScript/1_color_flipper/script.js
@@ -1,10 +1,9 @@
 // WHEN user clicks on the button,
-// THEN the background color of the body changes to a random color.
+// THEN the background color of the body changes to a random color selected from an array.
 //      The color name will also be displayed to the user.
 
-
+// ARRAY of pre-selected colors
 const colors = ["red", "green", "blue", "rgba(255,255,0)", "#A020F0"];
-
 
 // SELECTORS
 const button = document.querySelector('#button');
